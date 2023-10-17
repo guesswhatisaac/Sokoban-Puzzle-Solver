e709d3445a18ab21abcc4b329ea265a166bc4910
guesswhatisaac <guesswhatisaac@gmail.com>
2023-10-18 03:23:51 +0800

    Backtracking Implementation

--- src/solver/Search.java
+++ src/solver/Search.java
@@ -8,6 +8,7 @@ import java.util.PriorityQueue;
  * - In evaluateSuccessor(), the openList is iterated through to find matching nodes; can possibly be optimized
  */
 
+
 public class Search {
 
     private PriorityQueue<Node> openList = new PriorityQueue<>();
@@ -29,28 +30,28 @@ public class Search {
 
             Node currentNode = openList.poll();
 
-            // DEBUG ===============================
+            /*// DEBUG ===============================
             System.out.println("|Parent Node|");
             currentNode.toStringMap(map);
             currentNode.toStringInfo();
             System.out.println("current node pulled from open list");
-            //======================================
+            //======================================  */
 
             ArrayList<Node> successors = currentNode.generateSuccessors(rules, map);
+        
+            if (currentNode.isGoal(map)) {
+                actionList = backtrackPath(currentNode, closedList, map);
+                currentNode.toStringMap(map);
+                currentNode.toStringInfo();
+                return actionList;
+            }
 
             for (Node currentSuccessor : successors) {
     
-                //DEBUG ===============================
+                /* DEBUG ===============================
                 System.out.println("\n|successor node|");
                 currentSuccessor.toStringMap(map);
-                //====================================== 
-
-                if (currentSuccessor.isGoal(map)) {
-                    actionList = backtrackPath(currentSuccessor, closedListIdentifiers);
-                    currentSuccessor.toStringMap(map);
-                    currentSuccessor.toStringInfo();
-                    return actionList;
-                }
+                //====================================== */
 
                 //System.out.println("successor node is not the goal"); // DEBUG
 
@@ -72,29 +73,65 @@ public class Search {
         return actionList;
     }
 
-    private boolean evaluateSuccessor(Node successorNode) {
 
+    private boolean evaluateSuccessor(Node successorNode){
         int successorIdentifier = successorNode.getIdentifier();
+        boolean shouldAdd = true;
+        PriorityQueue<Node> openListClone = new PriorityQueue<Node>(openList);
 
-        if (openListIdentifiers.contains(successorIdentifier)) {
-            // Check if the successor is already in the open list with a lower fCost
-            for (Node nodeCopy : openList) {
-                if (nodeCopy.getIdentifier() == successorIdentifier && nodeCopy.getFCost() < successorNode.getFCost()) {
-                    return false;
+        // Check if the successor is already in the open list with a lower fCost
+        if(openListIdentifiers.contains(successorIdentifier)){
+            for(int i = 0; i < openListClone.size(); i++){
+                Node nodeCopy = openListClone.poll();
+                if(nodeCopy.getIdentifier() == successorNode.getIdentifier())
+                    shouldAdd = false;
+            }
+        }
+
+        // Check if the successor is already in the closed list with a lower fCost
+        if(shouldAdd && closedListIdentifiers.contains(successorIdentifier)){
+            for (Node closedListNode : closedList) {
+                if (closedListNode.getIdentifier() == successorIdentifier && closedListNode.getFCost() < successorNode.getFCost()) {
+                    shouldAdd = false;
+                    break;
                 }
             }
         }
 
-        if (closedListIdentifiers.contains(successorIdentifier)) {
-            // Check if the successor is already in the closed list with a lower fCost
-            return false;
-        }
-
-        return true;
+        return shouldAdd;
     }
 
-    public String backtrackPath(Node goalNode, HashSet<Integer> closedListIdentifiers) {
-        // TODO: Implement path backtracking
+    public String backtrackPath(Node goalNode, ArrayList<Node> closedList, MapData map) {
+        StringBuilder actions = new StringBuilder();
+        Node currentNode = goalNode;
+    
+        while (currentNode.getActionUsed() != '\0') {
+
+            // Find the parent node in the closed list
+            Node parent = findNodeInClosedList(currentNode, closedList, map);
+
+            // Retrieve the action that led to the current node
+            char action = currentNode.getActionUsed();
+            actions.insert(0, action);
+
+            // Move to the parent node for the next iteration
+            currentNode = parent;
+        }
+    
+        return actions.toString();
+    }
+
+    public Node findNodeInClosedList(Node current, ArrayList<Node> closedList, MapData map) {
+
+        for (Node node : closedList){
+            if (current.getParentIdentifier() == node.getIdentifier())
+                return node;
+        }
+
+        // parent node is not found, 
+        System.out.println("Parent not found for identifier: " + current.getParentIdentifier());
+        current.toStringMap(map);
+        
         return null;
     }
 }
